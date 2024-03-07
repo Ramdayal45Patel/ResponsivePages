6e6a020e8dc416c0126ed19ec9c9e30b1a44203e
RamDayal Patel <ramdayal45120@gmail.com>
2024-03-07 23:45:27 +0530

    Comment Code

--- src/models/apiCount.tsx
+++ src/models/apiCount.tsx
@@ -1,28 +1,28 @@
-import React, { createContext, useContext, useState, useMemo } from "react";
+// import React, { createContext, useContext, useState, useMemo } from "react";
 
-// Create a context to hold the API call count
-const ApiCallCountContext = createContext<any>(0);
+// // Create a context to hold the API call count
+// const ApiCallCountContext = createContext<any>(0);
 
-// Custom hook to access the API call count and increment it
-export const useApiCallCount = () => useContext(ApiCallCountContext);
+// // Custom hook to access the API call count and increment it
+// export const useApiCallCount = () => useContext(ApiCallCountContext);
 
-// Provider component to manage the API call count state
-export const ApiCallCountProvider = ({ children, initialCount = 0 }:any) => {
-  const [apiCallCount, setApiCallCount] = useState(initialCount);
+// // Provider component to manage the API call count state
+// export const ApiCallCountProvider = ({ children, initialCount = 0 }:any) => {
+//   const [apiCallCount, setApiCallCount] = useState(initialCount);
 
-  // Function to increment the API call count
-  const incrementApiCallCount = () => {
-    setApiCallCount((prevCount:any) => prevCount + 1);
-  };
+//   // Function to increment the API call count
+//   const incrementApiCallCount = () => {
+//     setApiCallCount((prevCount:any) => prevCount + 1);
+//   };
 
-  const contextValue = useMemo(
-    () => ({ apiCallCount, incrementApiCallCount }),
-    [apiCallCount, incrementApiCallCount]
-  );
+//   const contextValue = useMemo(
+//     () => ({ apiCallCount, incrementApiCallCount }),
+//     [apiCallCount, incrementApiCallCount]
+//   );
 
-  return (
-    <ApiCallCountContext.Provider value={contextValue}>
-      {children}
-    </ApiCallCountContext.Provider>
-  );
-};
+//   return (
+//     <ApiCallCountContext.Provider value={contextValue}>
+//       {children}
+//     </ApiCallCountContext.Provider>
+//   );
+// };
